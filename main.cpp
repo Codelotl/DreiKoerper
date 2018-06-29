e0c3d6c5286276f69cbd11d389dfeed54aae90f2
Codelotl <1721498@uni-wuppertal.de>
2018-06-29 09:20:02 +0200

    Test again kinda

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
+#include "stdio.h"
+#include "math.h"
 #include <iostream>
-
+#include <vector>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<double> v;
+    v.push_back(2.0);
+    v.push_back(3.0);
     return 0;
 }

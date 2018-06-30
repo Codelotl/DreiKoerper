389c875ca752e17a48f33b21d80ec4827ce9e7f3
Codelotl <1721498@uni-wuppertal.de>
2018-07-01 00:43:07 +0200

    Test again kinda

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 #include "math.h"
 #include <iostream>
 #include <vector>
+using std::cout;
+using std::endl;
 int main() {
-    std::vector<double> v;
+    std::vector<double> v, u = {1,2}, t;
     v.push_back(2.0);
     v.push_back(3.0);
+    u[0] += v[0];
+    u[1] += v[1],
+    cout << u[0] << endl;
+    cout << u[1] << endl;
+    sqrt()
     return 0;
 }

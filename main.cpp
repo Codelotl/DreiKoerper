4187d17f787ba633652c5f91c92f5bd12aa51624
Codelotl <1721498@uni-wuppertal.de>
2018-07-03 23:59:25 +0200

    Implementierung einer Klasse von Gravitationskörpern mit Definitionen zur Bestimmung des Ortes, der Richtung und der Masse und der Möglichkeit diese zu setzen. main.cpp dient zur Zeit noch dazu alle Befehle einfach auf ihre Richtigkeit zu prüfen.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
-#include "stdio.h"
-#include "math.h"
+#include <stdio.h>
+#include <math.h>
+#include "Gravk.h"
 #include <iostream>
 #include <vector>
 using std::cout;
 using std::endl;
 int main() {
     std::vector<double> v, u = {1,2}, t;
+    double m= 1.0;
+    double m1, x1;
+    Gravk a;
+    a.setAll(v,v,v,v,m);
     v.push_back(2.0);
     v.push_back(3.0);
-    u[0] += v[0];
-    u[1] += v[1],
-    cout << u[0] << endl;
-    cout << u[1] << endl;
+    a.setAll(v,v,v,v,m);
+    m1 = a.getMass();
+    cout << m1 << endl;
+    cout << "X:" << a.getPosx(1) << "Y:" << a.getPosy(1) << endl;
+    cout << "VX:" << a.getVelx(1) << "VY:" << a.getVely(1) << endl;
     return 0;
 }

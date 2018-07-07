cd2e95e9b512b42c3410d7d61ba82d2c21699ed6
Codelotl <1721498@uni-wuppertal.de>
2018-07-07 16:58:47 +0200

    Geschw. funktion wurde verbessert. Neue Position kann danach berechnet werden. To-Do: -Counter, der feststellt wieviele Gravkörper im System sind -For Schleife, die automatisch die verschieden Gravkörper durchläuft sowohl als Ausgangskörper als auch als Ziel -Speichern der Daten für Grafikwiedergabe

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 using std::cout;
 using std::endl;
 int main() {
-    std::vector<double> v={0,0}, u={3.844e+2,3.844e+2}, t;
+    std::vector<double> v (100), u={3.844e+1,3.844e+1}, t={10e+10,10e+10}; //v (100) gibt vektor mit 100 null einträgen
     double m1= 5.972e+24;
-    double m2= 6.0e+10;
+    double m2= 6.0e+24;
     int time=1;
     int time1=2;
     Gravk a;
@@ -17,14 +17,17 @@ int main() {
     //v.push_back(2.0);
     //v.push_back(3.0);
     a.setAll(v,v,v,v,m1);
-    b.setAll(u,u,v,v,m2);
+    b.setAll(u,t,v,v,m2);
+    c.setAll(t,u,v,v,m1);
     cout << a.getMass() << endl;
     cout << "X:" << a.getPosx(1) << "Y:" << a.getPosy(1) << endl;
     cout << "VX:" << a.getVelx(1) << "VY:" << a.getVely(1) << endl;
     cout << a.distance(b,time) << endl;
     cout << "Kraft in x" << a.forcex(b,time) << "Kraft in y" << a.forcey(b,time) << endl;
-    a.newVel(b,time);
-    cout << a.getVelx(time) << endl << a.getVely(time) << endl;
-    cout << a.test(time);
+    //a.newVel(b,time);
+    a.setVel(b,time,time);
+    a.setVel(c,time,time);
+    a.newPos(time);
+    cout << "x=" << a.getPosx(time) << "y=" << a.getPosy(time) << endl;
     return 0;
 }

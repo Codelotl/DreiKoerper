ac6e3d6d6cc3d558014a6b278dfb49be1c9c40dd
Codelotl <1721498@uni-wuppertal.de>
2018-07-09 15:57:17 +0200

    Funktionierende Funktionen als nächstes Werte in einem Dokument speichern

--- Gravk.h
+++ Gravk.h
@@ -51,42 +51,45 @@ public:
     double getVely(unsigned t){ //Funktion um Y-Geschw. zur Zeit t zu kriegen
         return vy.at(t);
     }
-    double distance(Gravk a, int t){ //Berechnet Abstand von zwei Körpern zur Zeit t
+    double distance(Gravk a, unsigned t){ //Berechnet Abstand von zwei Körpern zur Zeit t
         return sqrt(pow((getPosx(t) - a.getPosx(t)),2)+pow((getPosy(t)-a.getPosy(t)),2));
     }
-    double forcex(Gravk a, int t){
+    double forcex(Gravk a, unsigned t){
         return (G*mass*a.getMass()*(getPosx(t)-a.getPosx(t)))/pow(distance(a,t),3);
     }
-    double forcey(Gravk a, int t){
+    double forcey(Gravk a, unsigned t){
         return (G*mass*a.getMass()*(getPosy(t)-a.getPosy(t)))/pow(distance(a,t),3);
     }
-    double Forcex(Gravk a[], int t, int bcount){
-        int i;
+    double Forcex(Gravk** body, unsigned t, int bcount){
         double force=0;
-        for(i=0;i<bcount;i++){
-        force = force+(G*mass*a[i].getMass()*(getPosx(t)-a[i].getPosx(t)))/pow(distance(a[i],t),3);
-            return force;
+        for(int i=0;i<bcount;i++){
+            double dis=distance((*body[i]),t-1);
+            if (dis>1000.0)
+        force = force+(G*mass*(*body[i]).getMass()*(getPosx(t-1)-(*body[i]).getPosx(t-1)))/pow(dis,3);
         }
+        return force;
     }
-    double Forcey(Gravk a[], int t, int bcount){
+    double Forcey(Gravk** body, unsigned t, int bcount){
         int i;
         double force=0;
         for(i=0;i<bcount;i++){
-            force = force+(G*mass*a[i].getMass()*(getPosy(t)-a[i].getPosy(t)))/pow(distance(a[i],t),3);
-            return force;
+            double dis=distance((*body[i]),t-1);
+            if (dis>1000.0)
+            force = force+(G*mass*(*body[i]).getMass()*(getPosy(t-1)-(*body[i]).getPosy(t-1)))/pow(dis,3);
         }
+        return force;
     }
     //void newVel(Gravk a, int t) {vx[t]=vx[t-1]+(this->forcex(a,t)*dt)/(mass), vy[t]=vy[t-1]+(this->forcey(a,t)*dt)/(mass);}
     //void velAdd(Gravk a, int t) {vx.at(t)+=vx.at(t)+(forcex(a,t)*dt)/(mass), vy.at(t)+=vy.at(t)+(forcey(a,t)*dt)/(mass);}
-    void setVel(Gravk a[], int t, int bcount){
-        vx.push_back(vx.at(t-1)+(this->Forcex(a,t,bcount)*dt)/(mass));
-        vy.push_back(vy.at(t-1)+(this->Forcey(a,t,bcount)*dt)/(mass));
+    void setVel(Gravk** body, unsigned t, int bcount){
+        vx.push_back(vx.at(t-1)+(this->Forcex(body,t,bcount)*dt)/(mass));
+        vy.push_back(vy.at(t-1)+(this->Forcey(body,t,bcount)*dt)/(mass));
     } //bcount anzahl der körper -1
-    void newPos(int t) {
+    void newPos(unsigned t) {
         x.push_back(x.at(t-1)+vx.at(t)*1); //ACHTUNG hier wird immer von Zeitschritten von einer sek ausgegangen
         y.push_back(y.at(t-1)+vy.at(t)*1);}
     double test(int t) {return mass;}
-    void print(int t) { cout << "Körper mit m = " << this->getMass() << ", v= (" << this->getVelx(t) << " , " << this->getVely(t) <<")"<< "; r= (" << this->getPosx(t) <<" , " << getPosy(t) <<")"<< endl;}
+    void print(int t) { cout << "Koerper mit m = " << this->getMass() << ", v= (" << this->getVelx(t) << " , " << this->getVely(t) <<")"<< "; r= (" << this->getPosx(t) <<" , " << getPosy(t) <<")"<< endl;}
 };
 
 
